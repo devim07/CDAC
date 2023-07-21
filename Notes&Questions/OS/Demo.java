046597b132e25ca10e85bc35ffdce91b94f73d6c
devim07 <94759318+devim07@users.noreply.github.com>
2023-07-21 21:08:07 +0530

    Add files via upload

---
+++ Notes&Questions/OS/Demo.java
@@ -0,0 +1,19 @@
+
+class OOME{
+	
+}
+
+class Demo{
+	
+	public static void main(String[] args){
+		//main(null);
+		//System.out.println("Reached here!!!");
+		
+		while(true){
+			new OOME();
+		}
+	}
+}
+
+//StackOverflowError
+//OutOfMemoryError
